8f781846d02341fa7f8af2790b1ff05e27f08c1a
Ritter Gustave <rGustave5@gmail.com>
2017-08-08 20:06:39 -0700

    Added prototype ajax call

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,47 @@
+// Javascript written by Ritter Gustave
+
+$.ajaxPrefilter(function(options) {
+    if (options.crossDomain && $.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
+var platform;
+$('#ps4').on('click', function(){
+  platform = "ps4";
+})
+$('#xbox').on('click', function(){
+  platform = "xbox";
+})
+$('#steam').on('click', function(){
+  platform = "steam";
+})
+
+
+var apikey = localStorage.getItem('apikey');
+console.log(apikey)
+if (!apikey) {
+  apikey = prompt('API key?');
+  localStorage.setItem('apikey', apikey);
+}
+$('#submit').on('click', function(){
+  var id = $('#steamID').val()
+
+  $.ajax({
+    method: 'GET',
+    url: "https://api.rocketleague.com/api/v1/steam/playerskills/" + id + "/",
+    headers: {
+      'Authorization': 'Token ' + apikey
+    }
+  }).done(function(data) {
+    console.log(data);
+    console.log('worked');
+  }, function(data) {
+    console.log('nope');
+  });
+  console.log('did the thing')
+});
+
+
+// GET: /api/v1/<platform>/playerskills/<player_id>/
+// POST: /api/v1/<platform>/playerskills
